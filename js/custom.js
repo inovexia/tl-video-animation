adc079ea8a444702dbf12ea1faa2c00e5c079ad3
Ankit Shukla <4nkit5hukla@gmail.com>
2021-03-25 15:17:58 +0530

    Changes By Ankit

--- js/custom.js
+++ js/custom.js
@@ -1,78 +1,94 @@
-(function($)
- {
- 
-$(document).ready(function() {
-  
-
-$(document).scroll(function () {  
-    let scroll = $(this).scrollTop();
-    let opacity = 1 - (scroll / 1000);
-    let opacity2 = 0 + (scroll / 1000);
-    
-    if (opacity > 0) {
-        $('.overlay').css('opacity', opacity);
-    }
-
-    $("section").each(function () {
-      if (
-        $(this).position().top <= $(document).scrollTop()    &&
-        $(this).position().top + $(this).outerHeight() > $(document).scrollTop()
-      ) {
-        let fixed = $(this).attr("id");
-        if (fixed == "section3") {
-          $("#section3").addClass("active-section");
-          $(".large-video").trigger('play');
-          
-        } 
-        else {
-          $("#section3").removeClass("active-section");
-          $(".large-video").trigger('pause');
-          
+(function($) {
+  $(document).ready(function() {
+    const win = $(window),
+      sectionWrapper = $('.section-video'),
+      contentWrapper = $('.sticky-content'),
+      introWrapper = $('.video-intro-wrapper'),
+      videoIntro1 = $('.video-intro-1'),
+      videoIntro2 = $('.video-intro-2'),
+      manifestoWrapper = $('.video-manifesto-wrapper'),
+      videoScrim = $('.video-scrim'),
+      totalScrollArea = sectionWrapper.height() - contentWrapper.height(),
+      videoOpacity = oValue => {
+        videoScrim.css({ opacity: oValue });
+      },
+      introOpacity = oValue => {
+        introWrapper.css({ opacity: oValue });
+      },
+      manifestoOpacity = oValue => {
+        manifestoWrapper.css({ opacity: oValue });
+      },
+      introTransform = ty => {
+        introWrapper.css({
+          transform: `matrix(1, 0, 0, 1, 0, ${ty})`
+        });
+      },
+      manifestoTransform = ty => {
+        manifestoWrapper.css({
+          transform: `matrix(1, 0, 0, 1, 0, ${ty})`
+        });
+      },
+      moveIntro = pageYOffset => {
+        console.log(
+          pageYOffset,
+          introWrapper.height(),
+          manifestoWrapper.height() * 1.5 - pageYOffset,
+          (manifestoWrapper.height() * 1.5 - pageYOffset / 1.8) / 1.8
+        );
+        let oValue;
+        if (
+          pageYOffset >=
+          introWrapper.height() + introWrapper.height() / 1.2
+        ) {
+          console.log(1);
+          videoOpacity(0.8);
+          manifestoOpacity(1);
+          manifestoTransform(manifestoWrapper.height() * 3 - pageYOffset);
         }
+        if (
+          pageYOffset >= introWrapper.height() + introWrapper.height() / 12 &&
+          pageYOffset < introWrapper.height() + introWrapper.height() / 1.2
+        ) {
+          console.log(2);
+          manifestoOpacity(0);
+          // manifestoTransform(introWrapper.height() / 1.5);
+          manifestoTransform(manifestoWrapper.height() * 1.5);
+          videoIntro2.removeClass('hidden');
+          videoOpacity(0);
+        }
+        if (
+          pageYOffset >= introWrapper.height() &&
+          pageYOffset < introWrapper.height() + introWrapper.height() / 12
+        ) {
+          videoIntro1.removeClass('hidden');
+          videoIntro2.addClass('hidden');
+          introOpacity(1);
+          videoOpacity(0.8);
+        }
+        if (pageYOffset >= 400 && pageYOffset < introWrapper.height()) {
+          introOpacity(0.15);
+          videoIntro1.addClass('hidden');
+          videoOpacity(1);
+        }
+        if (pageYOffset == 0) {
+          introOpacity(0);
+        }
+        introOpacity(oValue);
+        introTransform((introWrapper.height() - pageYOffset) / 1.8);
+      },
+      init = () => {
+        videoOpacity(1);
+        introTransform(introWrapper.height() / 1.8);
+        introOpacity(0);
+        manifestoTransform(manifestoWrapper.height() * 1.5);
+        manifestoOpacity(0);
+      };
+    init();
+    win.scroll(function(e) {
+      if (e.currentTarget.pageYOffset === 0) {
+        videoOpacity(1);
       }
+      moveIntro(e.currentTarget.pageYOffset);
     });
-
-          let mm = $(window).scrollTop();
-       
-          if(mm > 80)
-          {
-            $("#section3 .low-light").css('opacity','1');  
-          }
-          else {
-            $("#section3 .low-light").css('opacity','0.8');
-          
-          }
-          if(mm > 100) {$("#section3 .video-intro-1").css('opacity','1');  }
-          else{
-            $("#section3 .video-intro-1").css('opacity','0');
-          }
-         
-          if(mm > 175) {$("#section3 .video-intro-2").css('opacity','1');
-         }
-          else {
-            $("#section3 .video-intro-2").css('opacity','0');
-          }
-          if(mm > 250) {
-          $('.overlay').css('opacity', 0); 
-         }
-         if(mm > 350) {
-          $('.block-1').css('opacity', 0); 
-         }
-         else{
-          $('.block-1').css('opacity', 1); 
-         }
-         
-         
-          
-  }); 
-
-
-  
-
-
-  
   });
 })(jQuery);
-
-
-
